c8e75cd39fdadeaf1b48b1c068d54fddc9fc43c5
mhuzaifa125 <97709057+mhuzaifa125@users.noreply.github.com>
2022-02-25 16:21:04 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MarkdownParseTest {
-    @Test
+    /*@Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
@@ -51,5 +51,40 @@ public class MarkdownParseTest {
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = MarkdownParse.getLinks(contents);
         assertEquals("Test links in format2.md including link with space after brackets", List.of(), links);
+    }*/
+
+    @Test
+    public void testSnippet1() throws IOException {
+        Path fileName = Path.of("snippet1.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        // Expected output
+        ArrayList<String> expectedoutput = new ArrayList<>();
+        expectedoutput.add("`google.com");
+        expectedoutput.add("google.com");
+        expectedoutput.add("ucsd.edu");
+        assertEquals(expectedoutput, links);
+    }
+    @Test
+    public void testSnippet2() throws IOException {
+        Path fileName = Path.of("snippet2.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        // Expected output
+        ArrayList<String> expectedoutput = new ArrayList<>();
+        expectedoutput.add("a.com");
+        expectedoutput.add("a.com(())");
+        expectedoutput.add("example.com");
+        assertEquals(expectedoutput, links);
+    }
+    @Test
+    public void testSnippet3() throws IOException {
+        Path fileName = Path.of("snippet3.md");
+        String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        // Expected output
+        ArrayList<String> expectedoutput = new ArrayList<>();
+        expectedoutput.add("https://ucsd-cse15l-w22.github.io/");
+        assertEquals(expectedoutput, links);
     }
 }
